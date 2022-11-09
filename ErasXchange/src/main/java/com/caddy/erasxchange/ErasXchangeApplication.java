c96d1a7c1ddb81e07cb6c9a049da8dfc75259a73
Ali Güzey <aliemirguzey@gmail.com>
2022-11-09 22:42:57 +0300

    Start Spring project

---
+++ ErasXchange/src/main/java/com/caddy/erasxchange/ErasXchangeApplication.java
@@ -0,0 +1,13 @@
+package com.caddy.erasxchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErasXchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErasXchangeApplication.class, args);
+    }
+
+}
---
+++ ErasXchange/src/test/java/com/caddy/erasxchange/ErasXchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caddy.erasxchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErasXchangeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
