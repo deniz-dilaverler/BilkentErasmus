b3d57dba9a7510a6471da1f6d1fdf9d35179e280
Dethmaw <daghanunal20@gmail.com>
2022-12-04 17:38:31 +0300

    initial commit

---
+++ src/Sidebar/SidebarData.js
@@ -0,0 +1,40 @@
+import React from "react";
+import HomeIcon from "@mui/icons-material/Home";
+import SchoolIcon from '@mui/icons-material/School';
+import BorderColorIcon from '@mui/icons-material/BorderColor';
+import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
+import PersonIcon from "@mui/icons-material/Person";
+import LockIcon from "@mui/icons-material/Lock";
+
+export const SidebarData = [
+  {
+    title: "Dashboard",
+    icon: <HomeIcon />,
+    link: "/dashboard",
+  },
+  {
+    title: "Schools",
+    icon: <SchoolIcon />,
+    link: "/schools",
+  },
+  {
+    title: "Application",
+    icon: <BorderColorIcon />,
+    link: "/application",
+  },
+  {
+    title: "Courses ",
+    icon: <HistoryEduIcon />,
+    link: "/courses",
+  },
+  {
+    title: "Profile",
+    icon: <PersonIcon />,
+    link: "/profile",
+  },
+  {
+    title: "Log out",
+    icon: <LockIcon />,
+    link: "/logout",
+  },
+];
