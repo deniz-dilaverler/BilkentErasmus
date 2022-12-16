ad51fab136ac761fc6925ae475c7be3fd5ee386d
Dağhan Ünal <daghanunal20@gmail.com>
2022-12-17 02:02:48 +0300

    added "add course" form

--- frontend/src/Sidebar/SidebarData.js
+++ frontend/src/Sidebar/SidebarData.js
@@ -4,7 +4,6 @@ import SchoolIcon from '@mui/icons-material/School';
 import BorderColorIcon from '@mui/icons-material/BorderColor';
 import HistoryEduIcon from '@mui/icons-material/HistoryEdu';
 import PersonIcon from "@mui/icons-material/Person";
-import LockIcon from "@mui/icons-material/Lock";
 
 export const SidebarData = [
   {
