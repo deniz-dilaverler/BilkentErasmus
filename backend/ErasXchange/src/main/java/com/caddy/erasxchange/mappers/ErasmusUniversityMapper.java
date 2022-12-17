8df6e2900a6c26f576fb05067e5afe938e87b2d1
CerenAkyr <cerenakyar@gmail.com>
2022-12-17 21:34:25 +0300

    Update ErasmusUniversityMapper.java

--- backend/ErasXchange/src/main/java/com/caddy/erasxchange/mappers/ErasmusUniversityMapper.java
+++ backend/ErasXchange/src/main/java/com/caddy/erasxchange/mappers/ErasmusUniversityMapper.java
@@ -1,60 +1,15 @@
 package com.caddy.erasxchange.mappers;
 
 import com.caddy.erasxchange.DTOs.AddErasmusUniversityDto;
-import com.caddy.erasxchange.DTOs.ErasmusUniversityDto;
-import com.caddy.erasxchange.models.course.ExternalCourse;
 import com.caddy.erasxchange.models.university.ErasmusUniversity;
-import com.caddy.erasxchange.models.users.Coordinator;
-import com.caddy.erasxchange.services.courses.ExternalCourseService;
-import com.caddy.erasxchange.services.user.CoordinatorService;
 import org.mapstruct.*;
 
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
-
-@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring",
-        uses = {ProgramMapper.class, CoordinatorService.class, ExternalCourseService.class})
+@Mapper(unmappedTargetPolicy = ReportingPolicy.IGNORE, componentModel = "spring")
 public interface ErasmusUniversityMapper {
-    @Mapping(source = "courseIds", target = "courses")
-    @Mapping(source = "coordinatorIds", target = "coordinators")
-    ErasmusUniversity toEntity(ErasmusUniversityDto erasmusUniversityDto);
+    ErasmusUniversity toEntity(AddErasmusUniversityDto addErasmusUniversityDto);
 
-    List<ErasmusUniversity> toEntityList(List<ErasmusUniversityDto> erasmusUniversityDtos);
-
-    // @Mapping(target = "courseIds", expression = "java(coursesToCourseIds(erasmusUniversity.getCourses()))")
-    @Mapping(source = "courses", target = "courseIds")
-    @Mapping(source = "coordinators", target = "coordinatorIds")
-    @Mapping(target = "isErasmus", constant = "true")
-    ErasmusUniversityDto toDto(ErasmusUniversity erasmusUniversity);
-
-    List<ErasmusUniversityDto> toDtoList(List<ErasmusUniversity> erasmusUniversity);
+    AddErasmusUniversityDto toDto(ErasmusUniversity erasmusUniversity);
 
     @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    ErasmusUniversity partialUpdate(ErasmusUniversityDto erasmusUniversityDto, @MappingTarget ErasmusUniversity erasmusUniversity);
-
-
-
-
-    default Set<Long> coursesToCourseIds(Set<ExternalCourse> courses) {
-        return courses.stream().map(ExternalCourse::getId).collect(Collectors.toSet());
-    }
-
-    default Set<Long> map(Set<Coordinator> coordinators) {
-        return coordinators.stream().map(Coordinator::getId).collect(Collectors.toSet());
-    }
-
-    @Mapping(source = "coordinatorIds", target = "coordinators")
-    @Mapping(source = "programs", target = "programs")
-    ErasmusUniversity addToEntity(AddErasmusUniversityDto addErasmusUniversityDto);
-
-
-    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
-    ErasmusUniversity partialUpdate1(AddErasmusUniversityDto addErasmusUniversityDto, @MappingTarget ErasmusUniversity erasmusUniversity);
-            /*
-    default Set<Long> coordinatorsToCoordinatorIds(Set<Coordinator> coordinators) {
-        return coordinators.stream().map(Coordinator::getId).collect(Collectors.toSet());
-    }
-
-     */
+    ErasmusUniversity partialUpdate(AddErasmusUniversityDto addErasmusUniversityDto, @MappingTarget ErasmusUniversity erasmusUniversity);
 }
