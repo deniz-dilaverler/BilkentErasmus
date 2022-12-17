4458fe25012c10150913d4d28252b61e920dff51
Ali Güzey <aliemirguzey@gmail.com>
2022-12-17 18:14:54 +0300

    Update Auth

--- backend/ErasXchange/src/main/java/com/caddy/erasxchange/controllers/UserController.java
+++ backend/ErasXchange/src/main/java/com/caddy/erasxchange/controllers/UserController.java
@@ -3,6 +3,10 @@ package com.caddy.erasxchange.controllers;
 import com.caddy.erasxchange.DTOs.LoginDto;
 import com.caddy.erasxchange.models.users.User;
 import com.caddy.erasxchange.security.AuthService;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -20,8 +24,9 @@ public class UserController {
     }
 
     @PostMapping()
-    public ResponseEntity<String> login(@RequestBody LoginDto loginDto) {
-        return authService.login(loginDto) ;
+    @ResponseBody
+    public ResponseTransfer login(@RequestBody LoginDto loginDto) {
+        return new ResponseTransfer(authService.login(loginDto)) ;
     }
 
     @GetMapping
@@ -29,4 +34,13 @@ public class UserController {
     public List<User> getAllUsers() {
         return authService.getAll();
     }
+}
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+class ResponseTransfer {
+    private String authToken;
+
+    // standard getters/setters
 }
