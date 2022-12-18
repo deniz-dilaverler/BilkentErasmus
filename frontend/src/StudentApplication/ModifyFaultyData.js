b35fd06165876d4e54b42b531dd57e2ef0ed40f1
CerenAkyr <cerenakyar@gmail.com>
2022-12-18 23:55:25 +0300

    Change Application Semester Implementation

--- frontend/src/StudentApplication/ModifyFaultyData.js
+++ frontend/src/StudentApplication/ModifyFaultyData.js
@@ -1,21 +1,56 @@
-import React from 'react';
+import { useState } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
 import Container from 'react-bootstrap/Container';
 import StudentAppItem from './StudentAppItem';
 import './ModifyFaultyData.css'
+import Button from '@mui/material/Button';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
 
 function ModifyFaultyData(props) {
 
+  const [open, setOpen] = useState(false);
+  const [openSemester, setOpenSemester] = useState(false);
+
+    const handleAllClickOpen = () => {
+        setOpen(true);
+      };
+
+      const handleClose = () => {
+        setOpen(false);
+      };
+
+      const handleSemesterClickOpen = () => {
+        setOpenSemester(true);
+      };
+
+      const handleSemesterClose = () => {
+        setOpenSemester(false);
+      };
+
   const cancelCurrentApplicationHandler = () => {
-    //TODO Cancel application in database!!!
-    // tekrar yüklenmesi de lazım sayfanın
+        const statusData = {
+          statType: "CURRENT",
+          no: props.no }
+        props.cancelApplicationStatusData(statusData);
+        console.log("Cancel current application!")
+        setOpen(false);
+        window.location.reload(false);
   }
 
   const changeSemesterHandler = () => {
-    //TODO Cancel application in database!!!
-    // tekrar yüklenmesi de lazım sayfanın
+    const statusData = {
+      statType: "SEMESTER_CHANGE",
+      no: props.no }
+    props.cancelApplicationStatusData(statusData);
+    console.log("CHANGE SEMESTER ULAN!!!")
+    setOpenSemester(false);
+    window.location.reload(false);
   }
 
     return (
@@ -38,13 +73,45 @@ function ModifyFaultyData(props) {
           <Row>
             <Col></Col>
             <Col>
-            <button onClick={cancelCurrentApplicationHandler}>Cancel Current Application</button>
+            <button onClick={handleAllClickOpen}>Cancel Current Application</button>
             </Col>
             <Col>
-            <button onClick={changeSemesterHandler}>Change Semester</button>
+            <button onClick={handleSemesterClickOpen}>Change Semester</button>
             </Col>
             <Col></Col>
-          </Row>
+            <Dialog open={open} onClose={handleClose}>
+            <DialogTitle >
+                    Cancel Current Application?
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure do you want to cancel the current application?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleClose}>Disagree</Button>
+                    <Button onClick={cancelCurrentApplicationHandler} autoFocus>
+                        Agree
+                    </Button>
+                 </DialogActions>
+                </Dialog>
+                <Dialog open={openSemester} onClose={handleSemesterClose}>
+            <DialogTitle >
+                    Change Current Application Semester?
+                </DialogTitle>
+                <DialogContent>
+                    <DialogContentText>
+                        Are you sure do you want to change current application semester?
+                    </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                    <Button onClick={handleSemesterClose}>Disagree</Button>
+                    <Button onClick={changeSemesterHandler} autoFocus>
+                        Agree
+                    </Button>
+                 </DialogActions>
+                </Dialog>
+                </Row>
         </Container>
       );
 }
