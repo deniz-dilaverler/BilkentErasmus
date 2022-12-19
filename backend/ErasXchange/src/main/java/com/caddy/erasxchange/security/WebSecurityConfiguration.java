5356fc9df662b107e69e9e941d0a4f847203841c
Ali Emir Güzey <82536335+guezey@users.noreply.github.com>
2022-12-19 06:54:38 +0300

    Çok şükür

--- backend/ErasXchange/src/main/java/com/caddy/erasxchange/security/WebSecurityConfiguration.java
+++ backend/ErasXchange/src/main/java/com/caddy/erasxchange/security/WebSecurityConfiguration.java
@@ -39,7 +39,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http
                 .csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/api/login").permitAll()
+                .antMatchers("/**").permitAll()
                 .antMatchers("/swagger-ui").permitAll()
                 .anyRequest()
                 .authenticated()
